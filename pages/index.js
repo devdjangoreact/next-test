41727ba6e5bcb51a10096554738cfb7f0b5ab9d3
devdjangoreact <devdjangoreact@gmail.com>
2023-03-11 02:20:21 +0000

    asdasdasd

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import MainContainer from "../components/MainContainer";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import App from "./graphql/main";
+
+const client = new ApolloClient({
+  uri: "http://192.168.17.133:5000/graphql",
+  cache: new InMemoryCache(),
+});
 
 const index = () => {
   return (
-    <MainContainer keywords={"main page"}>
-      <h1>Головна</h1>
-    </MainContainer>
+    <ApolloProvider client={client}>
+      <MainContainer keywords={"main page"}>
+        <h1>Головна</h1>
+        <App />
+      </MainContainer>
+    </ApolloProvider>
   );
 };
 
